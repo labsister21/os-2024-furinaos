4dd7c34499cb3fcf064531ceaf08895777e9ea0a
wisye <lunarwisyendra@gmail.com>
2024-04-16 16:17:37 +0700

    Nya! Inserted a new inserter for improved efficiency (ฅωฅ)ついでに GDT.c, GDT.h, interrupt.h, and interrupt.s also got some love! ✨

--- src/gdt.c
+++ src/gdt.c
@@ -1,4 +1,5 @@
 #include "header/cpu/gdt.h"
+#include "header/cpu/interrupt.h"
 
 /**
  * global_descriptor_table, predefined GDT.
@@ -52,6 +53,21 @@ struct GlobalDescriptorTable global_descriptor_table = {
             .granularity = 1,
             .base_high =0
         },
+        [5] = {
+            .base_high      = (sizeof(struct TSSEntry) & (0xF << 16)) >> 16,
+            .segment_low       = sizeof(struct TSSEntry),
+            .base_high         = 0,
+            .base_mid          = 0,
+            .base_low          = 0,
+            .non_system        = 0,    // S bit
+            .type_bit          = 0x9,
+            .descriptor_privillege_level = 0,    // DPL
+            .avl         = 1,    // P bit
+            .D_B        = 1,    // D/B bit
+            .L         = 0,    // L bit
+            .granularity       = 0,    // G bit
+        },
+        {0}
     }
 };
 
@@ -64,3 +80,10 @@ struct GDTR _gdt_gdtr = {
     .size = sizeof(global_descriptor_table) -1,
     .address = &global_descriptor_table
 };
+
+void gdt_install_tss(void) {
+    uint32_t base = (uint32_t) &_interrupt_tss_entry;
+    global_descriptor_table.table[5].base_high = (base & (0xFF << 24)) >> 24;
+    global_descriptor_table.table[5].base_mid  = (base & (0xFF << 16)) >> 16;
+    global_descriptor_table.table[5].base_low  = base & 0xFFFF;
+}
